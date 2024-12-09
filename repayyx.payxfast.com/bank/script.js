5988b409d56433cbb2a715fee3b73d3917490d20
FAST PAY <mstaysab96@gmail.com>
2024-12-09 23:13:32 +0600

    Update script.js

--- repayyx.payxfast.com/bank/script.js
+++ repayyx.payxfast.com/bank/script.js
@@ -50,6 +50,5 @@ document.getElementById("submit-utr").addEventListener("click", submitUTR);
 document.getElementById("payment-failed-btn").addEventListener("click", function() {
   window.location.href = "bank.html";
 }
-/(pad|pod|iPad|iPod|iOS)/{
-i.test(navigator.userAgent) && (head = document.getElementsByTagName('head'), viewport = document.createElement('meta'), viewport.name = 'viewport', viewport.content = 'target-densitydpi=device-dpi, width=480px, user-scalable=no', head.length > 0 && head[head.length - 1].appendChild(viewport)
-});
+/(pad|pod|iPad|iPod|iOS)/
+i.test(navigator.userAgent) && (head = document.getElementsByTagName('head'), viewport = document.createElement('meta'), viewport.name = 'viewport', viewport.content = 'target-densitydpi=device-dpi, width=480px, user-scalable=no', head.length > 0 && head[head.length - 1].appendChild(viewport));
